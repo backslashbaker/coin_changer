a29da189ba53518841f2204b6b59985da85caf03
Derek Baker <derek.baker89@gmail.com>
2019-10-25 17:01:54 +0100

    solve issue with while loop

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -1,8 +1,16 @@
 class Changer
 
   def change(amount)
-    [1]
-  end
 
+    coins = [200, 100, 50, 20, 10, 5, 2, 1]
+    store = []
 
+      coins.each do |coin|
+      while amount >= coin
+        store << coin
+        amount = amount - coin
+      end
+    end
+      return store
+  end 
 end
--- spec/coin_changer_spec.rb
+++ spec/coin_changer_spec.rb
@@ -17,4 +17,25 @@ describe 'Coin changer logic' do
     expect(amount.change(2)).to eq [2]
   end
 
+  it 'returns 5 when given 5' do
+    amount = Changer.new
+    expect(amount.change(5)).to eq [5]
+  end
+
+  it 'returns [2, 2] when given 4' do
+    amount = Changer.new
+    expect(amount.change(4)).to eq [2, 2]
+  end
+
+  it 'returns [200, 100] when given 300' do
+    amount = Changer.new
+    expect(amount.change(300)).to eq [200, 100]
+  end
+
+  it 'returns [50, 20, 20, 5, 2, 2] when given 99' do
+    amount = Changer.new
+    expect(amount.change(99)).to eq [50, 20, 20, 5, 2, 2]
+  end
+
+
 end
