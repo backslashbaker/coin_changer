be2865f4c25bfc72851ce5f51220dd597cef83c2
Derek Baker <derek.baker89@gmail.com>
2019-10-29 15:49:54 +0000

    refactor test and edited rubocop config

--- spec/coin_changer_spec.rb
+++ spec/coin_changer_spec.rb
@@ -3,29 +3,26 @@
 require 'rspec'
 require './lib/coin_changer'
 
-describe 'Coin changer logic' do
-  before do
-    @amount = Changer.new
-  end
-
+RSpec.describe 'CoinChanger' do
+  let!(:amount) { Changer.new }
   it 'returns 1 when given 1' do
-    expect(@amount.change(1)).to eq [1]
+    expect(amount.change(1)).to eq [1]
   end
 
   it 'returns 2 when given 2' do
-    expect(@amount.change(2)).to eq [2]
+    expect(amount.change(2)).to eq [2]
   end
 
   it 'returns 5 when given 5' do
-    expect(@amount.change(5)).to eq [5]
+    expect(amount.change(5)).to eq [5]
   end
 
   it 'returns [2, 2] when given 4' do
-    expect(@amount.change(4)).to eq [2, 2]
+    expect(amount.change(4)).to eq [2, 2]
   end
 
   it 'returns [200, 100] when given 300' do
-    expect(@amount.change(300)).to eq [200, 100]
+    expect(amount.change(300)).to eq [200, 100]
   end
 
   it 'returns [50, 20, 20, 5, 2, 2] when given 99' do
