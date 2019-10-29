a51496419a993afb97378cc94eb92c2088117cd7
Derek Baker <derek.baker89@gmail.com>
2019-10-29 12:10:17 +0000

    fixes:#2 fixes indentation of code

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -1,16 +1,16 @@
 class Changer
 
   def change(amount)
-
     coins = [200, 100, 50, 20, 10, 5, 2, 1]
     store = []
 
-      coins.each do |coin|
+    coins.each do |coin|
       while amount >= coin
         store << coin
         amount = amount - coin
       end
     end
-      return store
-  end 
+    return store
+  end
+
 end
